c105d3bdba6abde299dd4eaeb8d2a1084ce103d9
simonD3V <72438458+simonD3V@users.noreply.github.com>
2021-05-24 17:02:12 +0200

    Update Visualisation.jsx

--- IHM_React_JS/src/components/Visualisation.jsx
+++ IHM_React_JS/src/components/Visualisation.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Avatar, Box, CircularProgress, List, ListItem, ListItemAvatar, ListItemText, makeStyles, Paper, Typography } from '@material-ui/core'
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 import { withRouter } from 'react-router-dom';
 import { Sigma, RandomizeNodePositions, RelativeSize } from 'react-sigma';
 import clsx from 'clsx';
@@ -35,7 +36,7 @@ const useStyles = makeStyles((theme) => ({
     width: 40,
     height: 40,
   },
-  shape_exemplaires: {
+  shape_editions: {
     backgroundColor: '#80624D',
     width: 40,
     height: 40,
@@ -100,7 +101,7 @@ function FacebookCircularProgress(props) {
     </div>
   );
 }
-function Visualisation() {
+function Visualisation({ history, match }) {
   const classes = useStyles();
 
   // const [data, setData] = useState([])
@@ -293,19 +294,19 @@ function Visualisation() {
       for (var n = 0; n < nodes.length; n++) {
         for (var i = 0; i < d[nodes[n]].length; i++) {
           if (nodes[n] === 'airs') {
-            res = res.concat('{ "id" : "' + d[nodes[n]][i]['id'] + '", "label" : "' + d[nodes[n]][i]['air_normalise'] + '", "color" : "' + color[n] + '" }')
+            res = res.concat('{ "id" : "airs__' + d[nodes[n]][i]['id'] + '", "label" : "' + d[nodes[n]][i]['air_normalise'] + '", "color" : "' + color[n] + '" }')
           }
           if (nodes[n] === 'textes_publies') {
-            res = res.concat('{ "id" : "' + d[nodes[n]][i]['id'] + '", "label" : "' + d[nodes[n]][i]['titre'] + '", "color" : "' + color[n] + '" }')
+            res = res.concat('{ "id" : "textes_publies__' + d[nodes[n]][i]['id'] + '", "label" : "' + d[nodes[n]][i]['titre'] + '", "color" : "' + color[n] + '" }')
           }
           if (nodes[n] === 'references_externes') {
-            res = res.concat('{ "id" : "' + d[nodes[n]][i]['id'] + '", "label" : "' + d[nodes[n]][i]['titre'] + '", "color" : "' + color[n] + '" }')
+            res = res.concat('{ "id" : "references_externes__' + d[nodes[n]][i]['id'] + '", "label" : "' + d[nodes[n]][i]['titre'] + '", "color" : "' + color[n] + '" }')
           }
           if (nodes[n] === 'themes') {
-            res = res.concat('{ "id" : "' + d[nodes[n]][i]['id'] + '", "label" : "' + d[nodes[n]][i]['theme'] + '", "color" : "' + color[n] + '" }')
+            res = res.concat('{ "id" : "themes__' + d[nodes[n]][i]['id'] + '", "label" : "' + d[nodes[n]][i]['theme'] + '", "color" : "' + color[n] + '" }')
           }
           if (nodes[n] === 'editions') {
-            res = res.concat('{ "id" : "' + d[nodes[n]][i]['id'] + '", "label" : "' + d[nodes[n]][i]['titre_ouvrage'] + '", "color" : "' + color[n] + '" }')
+            res = res.concat('{ "id" : "editions__' + d[nodes[n]][i]['id'] + '", "label" : "' + d[nodes[n]][i]['titre_ouvrage'] + '", "color" : "' + color[n] + '" }')
           }
           if (i !== d[nodes[n]].length) {
             res = res.concat(', ')
@@ -322,40 +323,64 @@ function Visualisation() {
       for (var l = 0; l < links.length; l++) {
         for (i = 0; i < d[links[l]].length; i++) {
           if (links[l] === 'timbres') {
-            res = res.concat('{ "id" : "' + d[links[l]][i]['id'] + '", "source" : "' + d[links[l]][i]['airs']['id'] + '", "target" : "' + d[links[l]][i]['textes_publies']['id'] + '", "color" : "#CFCFCF" }, ')
+            res = res.concat('{ "id" : "' + d[links[l]][i]['id'] + '", "source" : "airs__' + d[links[l]][i]['airs']['id'] + '", "target" : "textes_publies__' + d[links[l]][i]['textes_publies']['id'] + '", "color" : "#CFCFCF" }, ')
           }
           if (links[l] === 'airs_references_externes') {
-            res = res.concat('{ "id" : "' + d[links[l]][i]['id'] + '", "source" : "' + d[links[l]][i]['airs']['id'] + '", "target" : "' + d[links[l]][i]['references_externes']['id'] + '", "color" : "#CFCFCF" }, ')
+            res = res.concat('{ "id" : "' + d[links[l]][i]['id'] + '", "source" : "airs__' + d[links[l]][i]['airs']['id'] + '", "target" : "references_externes__' + d[links[l]][i]['references_externes']['id'] + '", "color" : "#CFCFCF" }, ')
           }
           if (links[l] === 'editions_references_externes') {
-            res = res.concat('{ "id" : "' + d[links[l]][i]['id'] + '", "source" : "' + d[links[l]][i]['editions']['id'] + '", "target" : "' + d[links[l]][i]['references_externes']['id'] + '", "color" : "#CFCFCF" }, ')
+            res = res.concat('{ "id" : "' + d[links[l]][i]['id'] + '", "source" : "editions__' + d[links[l]][i]['editions']['id'] + '", "target" : "references_externes__' + d[links[l]][i]['references_externes']['id'] + '", "color" : "#CFCFCF" }, ')
           }
           if (links[l] === 'textes_publies_references_externes') {
-            res = res.concat('{ "id" : "' + d[links[l]][i]['id'] + '", "source" : "' + d[links[l]][i]['textes_publies']['id'] + '", "target" : "' + d[links[l]][i]['references_externes']['id'] + '", "color" : "#CFCFCF" }, ')
+            res = res.concat('{ "id" : "' + d[links[l]][i]['id'] + '", "source" : "textes_publies__' + d[links[l]][i]['textes_publies']['id'] + '", "target" : "references_externes__' + d[links[l]][i]['references_externes']['id'] + '", "color" : "#CFCFCF" }, ')
           }
           if (links[l] === 'textes_publies_themes') {
-            res = res.concat('{ "id" : "' + d[links[l]][i]['id'] + '", "source" : "' + d[links[l]][i]['textes_publies']['id'] + '", "target" : "' + d[links[l]][i]['themes_id']['id'] + '", "color" : "#CFCFCF" }, ')
+            res = res.concat('{ "id" : "' + d[links[l]][i]['id'] + '", "source" : "textes_publies__' + d[links[l]][i]['textes_publies']['id'] + '", "target" : "themes__' + d[links[l]][i]['themes']['id'] + '", "color" : "#CFCFCF" }, ')
           }
         }
       }
       // Il faut maintenant relier les textes à leur exemplaire respectif, on invente une id pour ces liens 
       for (var e = 0; e < d[nodes[1]].length; e++) {
-        res = res.concat('{ "id" : "' + e + '", "source" : "' + d[nodes[1]][e]['id'] + '", "target" : "' + d[nodes[1]][e]['edition']['id'] + '", "color" : "#CFCFCF" }, ')
+        res = res.concat('{ "id" : "' + e + '", "source" : "textes_publies__' + d[nodes[1]][e]['id'] + '", "target" : "editions__' + d[nodes[1]][e]['edition']['id'] + '", "color" : "#CFCFCF" }, ')
       }
       res = res.substring(0, res.length - 2)
       res = res.concat(']}')
       let res_json = JSON.parse(res)
       setDataTransformation(res_json)
-      console.log(res)
-      console.log(res_json)
+      // console.log(res)
+      // console.log(res_json)
 
     }
   }
   const circle_air = <div className={clsx(classes.shape_air, classes.shapeCircle)} />;
   const circle_textes_publies = <div className={clsx(classes.shape_textes_publies, classes.shapeCircle)} />;
-  const circle_exemplaires = <div className={clsx(classes.shape_exemplaires, classes.shapeCircle)} />;
+  const circle_editions = <div className={clsx(classes.shape_editions, classes.shapeCircle)} />;
   const circle_references = <div className={clsx(classes.shape_references, classes.shapeCircle)} />;
   const circle_themes = <div className={clsx(classes.shape_themes, classes.shapeCircle)} />;
+
+  function getRedirected() {
+    let nodes = ['airs', 'textes_publies', 'references_externes', 'themes', 'editions']
+
+    let table = clickedData[0].split('__')[0]
+
+    switch (table) {
+      case nodes[0]:
+        history.push('/single_air/' + clickedData[0].split('__')[1])
+        break
+      case nodes[1]:
+        history.push('/single_texte_publie/' + clickedData[0].split('__')[1])
+        break
+      case nodes[2]:
+        history.push('/single_reference/' + clickedData[0].split('__')[1])
+        break
+      case nodes[3]:
+        history.push('/single_theme/' + clickedData[0].split('__')[1])
+        break
+      case nodes[4]:
+        history.push('/single_edition/' + clickedData[0].split('__')[1])
+        break
+    }
+  }
 
   return (
     <div>
@@ -391,7 +416,7 @@ function Visualisation() {
                 <ListItem>
                   <ListItemAvatar>
                     <Avatar>
-                      {circle_exemplaires}
+                      {circle_editions}
                     </Avatar>
                   </ListItemAvatar>
                   <ListItemText primary="Editions" secondary={dataInter['editions'].length + " éléments"} />
@@ -471,22 +496,77 @@ function Visualisation() {
               Description de l'objet sélectionné
             </Paper>
             <Paper elevation={3} className={classes.paperWhite}>
-              {console.log(clickedData)}
               {clickedData.length === 0 ? (
                 <i>Aucune donnée sélectionnée</i>
               ) : (
                   <>
-                    <Typography variant='subtitle2' color='textSecondary' align='left'>
-                      Identifiant :
-                  </Typography>
-                    <Typography variant='body1' color='textPrimary' align='left'>
-                      {clickedData[0]}
-                      {console.log(dataInter)}
-                    </Typography>
-                    <Box pb={2} />
-                    <Typography variant='body1' color='textPrimary' align='left'>
-                      <i>{clickedData[1]}</i>
-                    </Typography>
+                    <Grid container spacing={2}
+                      direction="row"
+                      justify="right"
+                      alignItems="justify" >
+                      <Grid item xs={8}>
+                        <Box pb={2}>
+                          {clickedData[0].split('__')[0] === 'airs' && (<ListItem>
+                            <ListItemAvatar>
+                              <Avatar>
+                                {circle_air}
+                              </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="Airs"/>
+                          </ListItem>
+                          )}
+                          {clickedData[0].split('__')[0] === 'textes_publies' && (<ListItem>
+                            <ListItemAvatar>
+                              <Avatar>
+                                {circle_textes_publies}
+                              </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="Textes publiés"/>
+                          </ListItem>
+                          )}
+                          {clickedData[0].split('__')[0] === 'editions' && (<ListItem>
+                            <ListItemAvatar>
+                              <Avatar>
+                                {circle_editions}
+                              </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="Editions"/>
+                          </ListItem>
+                          )}
+                          {clickedData[0].split('__')[0] === 'references_externes' && (<ListItem>
+                            <ListItemAvatar>
+                              <Avatar>
+                                {circle_references}
+                              </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="Références externes"/>
+                          </ListItem>
+                          )}
+                          {clickedData[0].split('__')[0] === 'themes' && (<ListItem>
+                            <ListItemAvatar>
+                              <Avatar>
+                                {circle_themes}
+                              </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="Thèmes"/>
+                          </ListItem>
+                          )}
+                        </Box>
+                        <Typography variant='subtitle2' color='textSecondary' align='left'>
+                          Identifiant :
+                        </Typography>
+                        <Typography variant='body1' color='textPrimary' align='left'>
+                          {clickedData[0].split('__')[1]}
+                        </Typography>
+                        <Box pb={2} />
+                        <Typography variant='body1' color='textPrimary' align='left'>
+                          <i>{clickedData[1]}</i>
+                        </Typography>
+                      </Grid>
+                      <Grid item xs>
+                        <Button variant="contained" onClick={() => getRedirected()}>Consulter</Button>
+                      </Grid>
+                    </Grid>
                   </>
                 )}
             </Paper>
